e91d12cb9023a4f87388ba9f9b82102c4ee27503
PattyToledoP <patriciatoledop@hotmail.com>
2025-10-26 21:57:31 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,65 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1; 
+
+//código omitido
+
+// CRIAÇÃO  DE FUNÇÕES
+function exibirTextoNaTela(tag, texto) { 
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() { 
+    exibirTextoNaTela('h1', 'Jogo do número secreto'); 
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10'); I
+}
+
+function gerarNumeroAleatorio() {
+    //return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista=listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+      listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+     }
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+      exibirTextoNaTela('h1', 'Acertou!');
+      let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+      let mensagemTentativas = (`Voce descobriu o número secreto com ${tentativas} ${palavraTentativa}`);
+      exibirTextoNaTela('p', mensagemTentativas);
+      document.getElementById('reiniciar').removeAttribute('disabled');
+    } else{
+      if (chute > numeroSecreto) {
+        exibirTextoNaTela('p', 'O número secreto é menor.');
+      } else{
+        exibirTextoNaTela('p', 'O número secreto é maior.');
+      }
+      tentativas ++;
+      limpaCampo();
+    }
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limpaCampo();
+    tentativas =1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+exibirMensagemInicial();
+// EXECUÇÃO DAS FUNÇÕES 
+//exibirTextoNaTela('h1', 'Jogo do Número Secreto' );//executa a função
+//exibirTextoNaTela('p', 'Escolha número de 1 a 10' );//executa a função
